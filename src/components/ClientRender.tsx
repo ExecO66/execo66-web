b7dce43c39d3e587a9cf6bdbdb324887569021fa
Kevin <ge.kevin.a@gmail.com>
2022-02-27 20:55:56 -0600

    fix: hydration error in ClientRender

--- src/components/ClientRender.tsx
+++ src/components/ClientRender.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const ClientRender: FC = ({ children }) => {
-  const isServer = typeof window == "undefined";
+  const [isClient, setIsClient] = useState(false);
 
-  if (isServer) {
+  useEffect(() => {
+    setIsClient(typeof window != "undefined");
+  }, []);
+
+  if (!isClient) {
     return null;
   }
 
